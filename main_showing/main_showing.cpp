98bf863c1854849c0fbb064dfc56aa581a0c1794
Mark-std <.>
2025-03-22 23:45:31 +0200

    додано мейн+вивід всіх обєктів оренди

---
+++ main_showing/main_showing.cpp
@@ -0,0 +1,19 @@
+#include"../Building/Cont.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	Container container;
+
+	container.read_from_file("data.txt");
+
+	container.add_rent(Rent(4, 55.4, "Kovalya 2", 56.4, 4));
+	double total = container.calc_total_cost("Kovalchenka 1");
+	cout << "All rent building : " << endl;
+	for (int i = 0; i < container.get_top(); i++)
+	{
+		cout << container[i] << '\n';
+	}
+	cout << "Total street cost: " << total << endl;
+}
+
