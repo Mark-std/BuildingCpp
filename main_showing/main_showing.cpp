6d4bdb0d7ececb6c80d920774068cbb187e8b7cf
Mark-std <.>
2025-05-07 17:52:03 +0300

    do(menu)

--- main_showing/main_showing.cpp
+++ main_showing/main_showing.cpp
@@ -3,17 +3,36 @@
 using namespace std;
 int main()
 {
-	Container container;
+    Container container;
 
-	container.read_from_file("info.txt");
+    container.read_from_file("info.txt");
 
-	container.add_rent(Rent(4, 55.4, "Kovalya 2", 56.4, 4));
-	double total = container.calc_total_cost("Kovalcheyko 1");
-	cout << "All rent building : " << endl;
-	for (int i = 0; i < container.get_top(); i++)
-	{
-		cout << container[i] << '\n';
-	}
-	cout << "Total street cost: " << total << endl;
+    container.add_rent(Rent(4, 55.4, "Kovalya 2", 56.4, 4));
+    double total = container.calc_total_cost("Kovalcheyko 1");
+    cout << "All rent building : " << endl;
+    for (int i = 0; i < container.get_top(); i++)
+    {
+        cout << container[i] << '\n';
+    }
+    cout << "Total street cost: " << total << endl;
+
+    int choice;
+    string filename = "info.txt";
+
+    cout << "Rental Management Program" << endl;
+
+    do {
+        cout << "\nMenu:" << endl;
+        cout << "1. Load data from file" << endl;
+        cout << "2. Add new rental" << endl;
+        cout << "3. Calculate total cost by street" << endl;
+        cout << "4. Show all rental properties" << endl;
+        cout << "5. Save data to file" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Your choice: ";
+        cin >> choice;
+    } while (choice != 0);
+
 }
+
 
