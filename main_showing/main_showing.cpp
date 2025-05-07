1cd25149b7ad60c1fd3d1d98e12cbb527ca88455
Mark-std <.>
2025-05-07 19:22:20 +0300

    Fix switch

--- main_showing/main_showing.cpp
+++ main_showing/main_showing.cpp
@@ -27,86 +27,86 @@ int main()
         cout << "2. Add new rental" << endl;
         cout << "3. Calculate total cost by street" << endl;
         cout << "4. Show all rental properties" << endl;
-        cout << "5. Save data to file" << endl;
         cout << "0. Exit" << endl;
         cout << "Your choice: ";
         cin >> choice;
-    } 
-    switch (choice) {
-    case 1: {
-        cout << "Enter filename (default " << filename << "): ";
-        string input;
-        cin.ignore();
-        getline(cin, input);
-        if (!input.empty()) {
-            filename = input;
+
+        switch (choice) {
+        case 1: {
+            cout << "Enter filename (default " << filename << "): ";
+            string input;
+            cin.ignore();
+            getline(cin, input);
+            if (!input.empty()) {
+                filename = input;
+            }
+            container.read_from_file(filename);
+            cout << "Data loaded from file " << filename << endl;
+            break;
         }
-        container.read_from_file(filename);
-        cout << "Data loaded from file " << filename << endl;
-        break;
-    }
-    case 2: {
-        int area, rooms;
-        double price, meters;
-        string address;
+        case 2: {
+            int area, rooms;
+            double price, meters;
+            string address;
 
-        cout << "Enter address: ";
-        cin.ignore();
-        getline(cin, address);
+            cout << "Enter address: ";
+            cin.ignore();
+            getline(cin, address);
 
-        cout << "Enter number of rooms: ";
-        cin >> rooms;
+            cout << "Enter number of rooms: ";
+            cin >> rooms;
 
-        cout << "Enter area (sq.m): ";
-        cin >> meters;
+            cout << "Enter area (sq.m): ";
+            cin >> meters;
 
-        cout << "Enter price: ";
-        cin >> price;
+            cout << "Enter price: ";
+            cin >> price;
 
-        cout << "Enter floor: ";
-        cin >> area;
+            cout << "Enter floor: ";
+            cin >> area;
 
-        container.add_rent(Rent(rooms, price, address, meters, area));
-        cout << "New rental added successfully" << endl;
-        break;
-    } 
-    case 3: {
-        string street;
-        cout << "Enter street name to calculate total cost: ";
-        cin.ignore();
-        getline(cin, street);
-
-        double total = container.calc_total_cost(street);
-        cout << "Total rental cost for " << street << ": " << total << endl;
-        break;
-    }
-    case 4: {
-        cout << "All rental properties:" << endl;
-        for (int i = 0; i < container.get_top(); i++) {
-            cout << container[i] << '\n';
+            container.add_rent(Rent(rooms, price, address, meters, area));
+            cout << "New rental added successfully" << endl;
+            break;
         }
-        break;
-    }
-    case 5: {
-        cout << "Enter filename for saving (default " << filename << "): ";
-        string input;
-        cin.ignore();
-        getline(cin, input);
-        if (!input.empty()) {
-            filename = input;
+        case 3: {
+            string street;
+            cout << "Enter street name to calculate total cost: ";
+            cin.ignore();
+            getline(cin, street);
+
+            double total = container.calc_total_cost(street);
+            cout << "Total rental cost for " << street << ": " << total << endl;
+            break;
         }
-        // треба дописати метод 
-        //container.save_to_file(filename);
-        cout << "Data saved to file " << filename << endl;
-        break;
-    }
-    case 0:
-        cout << "Thank you for using the Rental Management Program!" << endl;
-        break;
-    default:
-        cout << "Invalid choice. Please try again." << endl;
-    }
-    while (choice != 0);
+        case 4: {
+            cout << "All rental properties:" << endl;
+            for (int i = 0; i < container.get_top(); i++) {
+                cout << container[i] << '\n';
+            }
+            break;
+        }
+              //      //  case 5: {
+              //          //  cout << "Enter filename for saving (default " << filename << "): ";
+              ////string input;
+              //         //   cin.ignore();
+              //         //   getline(cin, input);
+              //            if (!input.empty()) {
+              //                filename = input;
+              //            }
+              //            container.save_to_file(filename);
+              //            cout << "Data saved to file " << filename << endl;
+              //            break;
+              //        }
+        case 0:
+            cout << "Thank you for using the Rental Management Program!" << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 0);
+
+    return 0;
 }
 
 
